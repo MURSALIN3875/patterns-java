9429281d355d938eaa3b141ee57d3ecc2494afc6
MURSALIN3875 <sayyadmurslin@gmail.com>
2023-09-02 15:57:29 +0530

    0-1 Triangle Pattern

---
+++ zero_one_triangle.java
@@ -0,0 +1,21 @@
+public class zero_one_triangle
+{
+    public static void main(String arg[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                if((i+j)%2==0)
+                {
+                    System.out.print("1");
+                }
+                else
+                {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
