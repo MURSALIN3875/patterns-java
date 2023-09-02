0c1ad1666b83746575a79fecf8e479ed541e0e99
MURSALIN3875 <sayyadmurslin@gmail.com>
2023-09-02 15:47:33 +0530

    Floyd's Triangle Pattern

---
+++ floyds_triangle.java
@@ -0,0 +1,16 @@
+public class floyds_triangle
+{
+    public static void main(String arg[])
+    {
+        int n=1;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+              System.out.print(n+" ");
+              n++;
+            }
+            System.out.println();
+        }
+    }
+}
