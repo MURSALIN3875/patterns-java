4c42618819cad197f59892a101a5783057018a2e
MURSALIN3875 <sayyadmurslin@gmail.com>
2023-09-02 16:20:01 +0530

    Hollow Rhombus Pattern

---
+++ hollow_rhombus.java
@@ -0,0 +1,26 @@
+public class hollow_rhombus
+{
+    public static void main(String arg[])
+    {
+       int n=5;
+       for(int i=n;i>=1;i--)
+       {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1;j<=n;j++)
+        {
+            if(i==1||i==n||j==1||j==n)
+            {
+              System.out.print("*");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+       } 
+    }
+}
